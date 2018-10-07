18bdc696dab4ddd366ae94099346cc0ab79b1982
Yaso <syasoda@mikilab.doshisha.ac.jp>
2018-10-07 13:13:48 +0900

    first commit

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -1,5 +1,5 @@
 $(function() {
-    ws = new WebSocket("wss://desolate-dusk-67291.herokuapp.com:4000");
+    ws = new WebSocket("ws://desolate-dusk-67291.herokuapp.com:4000");
     
     var stock_request = {
         "stocks": ["1", "2", "3", "4", "5","6", "7", "8", "9", "10","11", "12", "13", "14", "15","16"]
